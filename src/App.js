54529b8a70daf9a9617349471c0c95a3c859c044
Saurav <90957348+saurav2528ara@users.noreply.github.com>
2024-02-05 19:14:53 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import React, { lazy, Suspense, useEffect, useState } from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./components/Header";
+import Body from "./components/Body";
+import { createRoutes, RouterProvider, Outlet } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
+
+import Contact from "./components/Contact";
+import error from "./components/Error";
+import Trouble from "./components/Error";
+import RestaurantMenu from "./components/RestroMenuPage";
+import UserContext from "./utils/userContext";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
+import Cart from "./components/Cart";
+
+// import Grocery from "./components/Grocery";
+const Grocery = lazy(() => import("./components/Grocery"));
+
+// import About from "./components/About";
+const About = lazy(() => import("./components/About"));
+
+const AppLayout = () => {
+  const [userName, setUserName] = useState();
+
+  // Authentication
+  useEffect(() => {
+    // Make an API call and send username and password
+
+    const data = {
+      name: "Kumar Saurav",
+    };
+    setUserName(data.name);
+  }, []);
+
+  return (
+    <Provider store={appStore}>
+      <UserContext.Provider value={{ loggedInUser: userName, setUserName }}>
+        <div className="app">
+          <Header />
+          <Outlet />
+        </div>
+      </UserContext.Provider>
+    </Provider>
+  );
+};
+
+const appRouter = createRoutes([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/grocery",
+        element: (
+          <Suspense fallback={<h1>Loading.........</h1>}>
+            <Grocery />
+          </Suspense>
+        ),
+      },
+      {
+        path: "/restaurants/:resId",
+        element: <RestaurantMenu />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+    ],
+    errorElement: <Trouble />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<RouterProvider router={appRouter} />);
